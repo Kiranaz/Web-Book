47c12a97118ad74247d2f648daf760561053376f
kiranaz <kiranaz_123@outlook.com>
2022-03-09 11:49:36 +0500

    initial action-creators structure

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom';
 import TextEditor from './components/text-editor';
 // import CodeCell from './components/code-cell';
+import { Provider } from 'react-redux';
+import { store } from './state';
 
 const App = () => {
   return (
+    <Provider store={store}>
     <div>
       {/* <CodeCell /> */}
       <TextEditor/>
-    </div>
+      </div>
+      </Provider>
   );
 };
 
